df69a6bd939cdf5c50c8d0c907e98c97a8e325c7
Alexander Vok <43250410+AlexanderVok@users.noreply.github.com>
2018-10-23 11:08:41 +0300

    Update main1.cpp

--- BT/main1.cpp
+++ BT/main1.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+#include "tree.h"
+using namespace std;
+int main() {
+    tree*root = new tree(5);
+    root->add(10);
+    root->add(2);
+    root->add(1);
+    root->add(12);
+    root->add(17);
+    root->add(9);
+    root->add(15);
+    root->print();
+    cout << endl << endl << endl;
+    root->del(10);
+    root->print();
+    cout << endl << endl << endl;
+    cout << "Searching " << " - "  <<root->search(12) << endl;
+    return 0;
+}
