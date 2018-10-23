a7d6e01147a0ce5dd4986ac7cadaaad737933193
Alexander Vok <43250410+AlexanderVok@users.noreply.github.com>
2018-10-23 11:10:12 +0300

    Update tree.cpp

--- BT/tree.cpp
+++ BT/tree.cpp
@@ -1,1 +1,147 @@
+#include "tree.h"
+#include <iostream>
+using namespace std;
 
+tree::tree() {
+    this->root = nullptr;
+}
+
+tree::tree(int value) {
+    this->root = new Node(value);
+}
+
+void tree::add(int value) {
+    if (root == nullptr) {
+        root = new Node(value);
+    }
+    else {
+        Node*current = this->root;
+        while (current != nullptr) {
+            if (value <= current->getValue()) {
+                if (current->getLeftSon() == NULL) {
+                    Node* newNode = new Node(value);
+                    current->setLeftSon(newNode);
+                    current = NULL;
+                }
+                else {
+                    current = current->getLeftSon();
+                }
+            }
+            else {
+                if (current->getRightSon() == NULL) {
+                    Node* newNode = new Node(value);
+                    current->setRightSon(newNode);
+                    current = NULL;
+                }
+                else {
+                    current = current->getRightSon();
+                }
+            }
+        }
+    }
+}
+
+void tree::print() {
+    this->root->print(root , 0);
+}
+
+void tree::del(int element){
+    if (root->getValue() == element)
+    {
+        Node*left_son = root->getLeftSon();
+        Node*right_son = root->getRightSon();
+        root->setLeftSon(NULL);
+        root->setRightSon(NULL);
+        delete root;
+        if (right_son)
+        {
+            root = right_son;
+            while (right_son->getLeftSon())
+            {
+                right_son = right_son->getLeftSon();
+            }
+            right_son->setLeftSon(left_son);
+        }
+        else
+        {
+            root = left_son;
+        }
+    }
+    else {
+        bool flag = false;
+        Node*parent = root;
+        Node*current = root;
+        while (current&&!flag) {
+            if (current->getValue() == element) {
+                flag = true;
+                int side = 0;
+                Node*left_son = current->getLeftSon();
+                Node*right_son = current->getRightSon();
+                current->setLeftSon(NULL);
+                current->setRightSon(NULL);
+                if (current->getValue() > parent->getValue()) {
+                    side = 1;
+                }
+                delete current;
+                if (side == 1) {
+                    if (right_son) {
+                        parent->setRightSon(right_son);
+                        while (right_son->getLeftSon())
+                        {
+                            right_son = right_son->getLeftSon();
+                        }
+                        right_son->setLeftSon(left_son);
+                    }
+                    else {
+                        parent->setRightSon(left_son);
+                    }
+                }
+                else {
+                    if (right_son) {
+                        parent->setLeftSon(right_son);
+                        while (right_son->getLeftSon())
+                        {
+                            right_son = right_son->getLeftSon();
+                        }
+                        right_son->setLeftSon(left_son);
+                    }
+                    else {
+                        parent->setLeftSon(left_son);
+                    }
+                }
+            }
+            else if(current->getValue() < element){
+                parent = current;
+                current = current->getRightSon();
+            }
+            else {
+                parent = current;
+                current = current->getLeftSon();
+            }
+        }
+        if (!flag) {
+            cout << "I can`t find element = " << element << endl;
+        }
+
+    }
+}
+
+bool tree::search(int element){
+    Node*hellper = root;
+    while (hellper != NULL)
+    {
+        if (hellper->getValue() == element)
+        {
+            return true;
+        }
+        else if (hellper->getValue() > element)
+        {
+            hellper = hellper->getLeftSon();
+        }
+        else
+        {
+            hellper = hellper->getRightSon();
+        }
+    }
+    return false;
+}
