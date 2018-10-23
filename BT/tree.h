195123449f7839aa63986ba314a846ffefe41d44
Alexander Vok <43250410+AlexanderVok@users.noreply.github.com>
2018-10-23 11:09:39 +0300

    Update tree.h

--- BT/tree.h
+++ BT/tree.h
@@ -1,1 +1,13 @@
+#include "Node.h"
 
+class tree {
+private:
+    Node*root;
+public:
+    tree();
+    tree(int value);
+    void add(int value);
+    void print();
+    void del(int element);
+    bool search(int element);
+};
