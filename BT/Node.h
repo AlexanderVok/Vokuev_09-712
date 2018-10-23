8c3fa2f2c9d6dc1eb41c3bd193229fbd86fb2e24
Alexander Vok <43250410+AlexanderVok@users.noreply.github.com>
2018-10-23 11:05:52 +0300

    Update Node.h

--- BT/Node.h
+++ BT/Node.h
@@ -1,1 +1,15 @@
-N
+class Node {
+private:
+    Node*leftSon;
+    Node*rightSon;
+    int value;
+public:
+    Node(int value);
+    void setLeftSon(Node *leftSon);
+    void setRightSon(Node *rightSon);
+    void setValue(int value);
+    Node*getLeftSon();
+    Node*getRightSon();
+    int getValue();
+    void print(Node *root,int level);
+};
