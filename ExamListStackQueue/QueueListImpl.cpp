9d02f89eadca259f0217e506799177879afe7982
Alexander Vok <43250410+AlexanderVok@users.noreply.github.com>
2018-12-25 09:47:31 +0300

    Create QueueListImpl.cpp

---
+++ ExamListStackQueue/QueueListImpl.cpp
@@ -0,0 +1,15 @@
+#include "QueueListImpl.h"
+QueueListImpl::QueueListImpl(IList *list) {
+    this->list = list;
+}
+void QueueListImpl::push(int value) {
+    this->list->addToBegin(value);
+}
+int QueueListImpl::pop() {
+    int value = this->list->get(this->list->size() - 1);
+    this->list->remove(this->list->size() - 1);
+    return value;
+}
+bool QueueListImpl::contains(int value) {
+    return this->list->contains(value);
+}
